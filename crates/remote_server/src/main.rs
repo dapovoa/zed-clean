dapovoa/zed-clean

commit f9d5c319d6c5b5f804423ac7461a7eb63e534d9c

Stop using xtask for clippy (#13223)
This fixes an extra 10 second delay when needing to recompile xtask, and
allows passing arbitrary clippy args (like --allow-dirty)

Release Notes:

- N/A

.github/workflows/ci.yml
@@ -51,6 +51,9 @@ jobs:
       - name: Check unused dependencies
         uses: bnjbvr/cargo-machete@main
 
+      - name: Check licenses are present
+        run: script/check-licenses
+
       - name: Check license generation
         run: script/generate-licenses /tmp/zed_licenses_output
 
@@ -87,7 +90,7 @@ jobs:
           clean: false
 
       - name: cargo clippy
-        run: cargo xtask clippy
+        run: ./script/clippy
 
       - name: Run tests
         uses: ./.github/actions/run_tests
@@ -114,7 +117,7 @@ jobs:
           clean: false
 
       - name: cargo clippy
-        run: cargo xtask clippy
+        run: ./script/clippy
 
       - name: Run tests
         uses: ./.github/actions/run_tests
@@ -139,7 +142,7 @@ jobs:
           save-if: ${{ github.ref == 'refs/heads/main' }}
 
       - name: cargo clippy
-        run: cargo xtask clippy
+        run: ./script/clippy
 
       - name: Build Zed
         run: cargo build -p zed

.github/workflows/deploy_collab.yml
@@ -27,7 +27,7 @@ jobs:
         uses: ./.github/actions/check_style
 
       - name: Run clippy
-        run: cargo xtask clippy
+        run: ./script/clippy
 
   tests:
     name: Run tests

.github/workflows/release_nightly.yml
@@ -32,7 +32,7 @@ jobs:
         uses: ./.github/actions/check_style
 
       - name: Run clippy
-        run: cargo xtask clippy
+        run: ./script/clippy
   tests:
     timeout-minutes: 60
     name: Run tests

script/check-licenses
@@ -1,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+check_license () {
+    for license in "LICENSE-AGPL" "LICENSE-GPL" "LICENSE-APACHE"; do
+        if [[ -L "$1/$license" ]]; then
+            return 0
+        elif [[ -e "$1/$license" ]]; then
+            echo "Error: $1/$license exists but is not a symlink."
+            exit 1
+        fi
+    done
+    echo "Error: $1 does not contain a license symlink"
+    exit 1
+}
+
+git ls-files **/*/Cargo.toml | while read cargo_toml; do
+   check_license $(dirname "$cargo_toml");
+done

script/clippy
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
-set -euxo pipefail
+set -euo pipefail
 
-cargo xtask clippy
+if [[ ! " $* " == *" -p "* && ! " $* " == *" --package "* ]]; then
+    set -- "$@" --workspace
+fi
+
+set -x
+"${CARGO:-cargo}" clippy "$@" --release --all-targets --all-features -- --deny warnings

commit 98abc9954b2cad8e53b322d08a3069d00ec7fda3

Run clippy for Windows (#15318)
This PR fixes running clippy on Windows, as it broke in #13223.

We can't run shell scripts on Windows, so we need to use something else.

Release Notes:

- N/A

.github/workflows/ci.yml
@@ -147,7 +147,8 @@ jobs:
           save-if: ${{ github.ref == 'refs/heads/main' }}
 
       - name: cargo clippy
-        run: ./script/clippy
+        # Windows can't run shell scripts, so we need to use `cargo xtask`.
+        run: cargo xtask clippy
 
       - name: Build Zed
         run: cargo build -p zed

crates/remote_server/src/main.rs
@@ -1,3 +1,5 @@
+#![cfg_attr(target_os = "windows", allow(unused, dead_code))]
+
 use fs::RealFs;
 use futures::channel::mpsc;
 use gpui::Context as _;
@@ -15,6 +17,12 @@ use std::{
     sync::Arc,
 };
 
+#[cfg(windows)]
+fn main() {
+    unimplemented!()
+}
+
+#[cfg(not(windows))]
 fn main() {
     env::set_var("RUST_BACKTRACE", "1");
     env_logger::builder()